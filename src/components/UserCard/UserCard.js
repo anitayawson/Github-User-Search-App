28d6814aed61cbb4c227db1b9951f2abc33dc786
Anita Yawson <anitayawson@gmail.com>
2024-01-23 02:23:30 -0500

    Refine responsive design: Adjust margins, paddings, typography, and icon sizes for different screen sizes

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -6,49 +6,60 @@ import companyIcon from "../../assets/icon-company.svg";
 
 export default function UserCard() {
   return (
-    <section class="user-card">
-      <div class="user__header">
-        <div class="user__img"></div>
-        <div class="user__header-info">
-          <h1 class="user__name">The Octocat</h1>
-          <h3 class="user__handle">@octocat</h3>
-          <h3 class="user__date-joined">Joined 25 Jan 2011</h3>
+    <section className="user-card">
+      <div className="user__header">
+        <div className="user__img"></div>
+        <div className="user__header-info">
+          <div>
+            <h1 className="user__name">The Octocat</h1>
+            <h3 className="user__handle">@octocat</h3>
+          </div>
+          <h3 className="user__date-joined">Joined 25 Jan 2011</h3>
         </div>
+        {/* <p className="">This profile has no bio</p> */}
       </div>
-      <p class="user__description">
+      <p className="user__description">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
         Quisque volutpat mattis eros.
       </p>
-      <div class="user-stats">
-        <div class="user-stats__repos">
-          <p class="user-stats__label">Repos</p>
-          <p class="user-stats__info">8</p>
+      <div className="user-stats">
+        <div className="user-stats__repos">
+          <p className="user-stats__label">Repos</p>
+          <p className="user-stats__info">8</p>
         </div>
-        <div class="user-stats__followers">
-          <p class="user-stats__label">Followers</p>
-          <p class="user-stats__info">3938</p>
+        <div className="user-stats__followers">
+          <p className="user-stats__label">Followers</p>
+          <p className="user-stats__info">3938</p>
         </div>
-        <div class="user-stats__following">
-          <p class="user-stats__label">Following</p>
-          <p class="user-stats__info">9</p>
+        <div className="user-stats__following">
+          <p className="user-stats__label">Following</p>
+          <p className="user-stats__info">9</p>
         </div>
       </div>
-      <div class="user-links">
-        <div class="user-links__location user-link__container">
-          <img class="location-icon" src={locationIcon} alt="location pin" />
-          <p class="user-link__name">San Francisco</p>
+      <div className="user-links">
+        <div>
+          <div className="user-links__location user-link__container">
+            <img
+              className="location-icon"
+              src={locationIcon}
+              alt="location pin"
+            />
+            <p className="user-link__name">San Francisco</p>
+          </div>
+          <div className="user-links__website user-link__container">
+            <img className="website-icon" src={websiteIcon} alt="website" />
+            <p className="user-link__name">https://github.blog</p>
+          </div>
         </div>
-        <div class="user-links__website user-link__container">
-          <img class="website-icon" src={websiteIcon} alt="website" />
-          <p class="user-link__name">https://github.blog</p>
-        </div>
-        <div class="user-links__social user-link__container">
-          <img class="social-icon" src={socialIcon} alt="twitter" />
-          <p class="user-link__name">Not Available</p>
-        </div>
-        <div class="user-links__company user-link__container">
-          <img src={companyIcon} alt="office building" />
-          <p class="user-link__name">@github</p>
+        <div>
+          <div className="user-links__social user-link__container">
+            <img className="social-icon" src={socialIcon} alt="twitter" />
+            <p className="user-link__name info-unavailable">Not Available</p>
+          </div>
+          <div className="user-links__company user-link__container">
+            <img src={companyIcon} alt="office building" />
+            <p className="user-link__name">@github</p>
+          </div>
         </div>
       </div>
     </section>
